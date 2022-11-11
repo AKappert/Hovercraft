7eecfea474b6e3c0c3e2476249801ab7ba189869
AKappert <andrewkappert@outlook.com>
2022-11-11 18:42:32 -0500

    Useless commit

--- main.ino
+++ main.ino
@@ -1,25 +1,10 @@
-//_-_-_-_-_-_-_-_-_-_-_-_-_ INCLUDES _-_-_-_-_-_-_-_-_-_-_-_-_\\ 
-#define __AVR_ATmega328__
-#include <Arduino.h>
-// #include <avr/iom328p.h>
-#include <avr/interrupt.h>
-#include <stdlib.h>
-#include <avr/io.h>
-#include <stdio.h>
 #include "Components/defines.h"
-///_-_-_-_-_-_-_-_-_-_-_-_-_ DEFINES _-_-_-_-_-_-_-_-_-_-_-_-_\\\ 
 
 
-#define SERVO_STEP_SIZE 1
-#define SERVO_CLUSTER_SIZE 3
 
-#define ECHO_PIN PD2
-#define TRIG_PIN PB5
 
-#define LED_PIN PB3
-#define NANO_LED_PIN PB5
-
-//_-_-_-_-_-_-_-_-_-_-_-_-_ VARIABLES _-_-_-_-_-_-_-_-_-_-_-_-_\\\ 
+//  ======================================== VARIABLES ========================================
+  
   
   // Enums
   enum MACHINE_STATE {SHUTDOWN, CALCULATING, MOVING, PAUSING};
@@ -50,77 +35,9 @@ static volatile struct {
 
 
 
-///_-_-_-_-_-_-_-_-_-_-_-_-_ CLASSES _-_-_-_-_-_-_-_-_-_-_-_-_\\\ 
- 
-class cLED {
-/*
-
-This class is used to represent an LED. It can either be a pwm controlled LED
-with adjustable brightness, or a discrete LED that can be on or off. This LED
-object will be responsible for initializing it's port pins, turning the object 
-on or off, and setting the brightness if possible.
-
-*/
-  private:
-    enum LED_TYPE led_type;
-    bool IS_ON = false;    //true = on.
-    float dim_value = 0;   //0-255.
-
-  public:
-  
-    //cLED default constructor
-    cLED() {
-      led_type = DISCRETE;
-    }
-
-    //cLED constructor with initializer. IDEALY USE THIS CONSTRUCTOR FOR DISCRETE LEDs
-    cLED(bool on, LED_TYPE l_type) {
-      IS_ON = on;
-      led_type = l_type;
-    }
-
-    //cLED constructor with initializer. IDEALY USE THIS CONSTRUCTOR FOR PWM LEDs
-    cLED(bool on, LED_TYPE l_type, float dimness) {
-      IS_ON = on;
-      led_type = l_type;
-      dim_value = dimness;
-    }
-
-    // Function used to set arduino ports
-    void initialize_LED(){
-
-    }
-
-    // Function used to set PWM dimness value
-    void set_dimness (float dimness){
-      dim_value = dimness;
-    }
-
-    // Function used to set the LED
-    void set_LED(){
-      // LED is dimmable since it is PWM
-      if (led_type == PWM){
-        
-      }
-      // LED can only be on / off since it is DISCRETE
-      else if (led_type == DISCRETE) {
-
-      }
-    }
 
 
-
-};
-
-// I'm thinking that storing the list of distance values with the servo angle as a heap might work
-class cMAX_HEAP {
-  
-};
-
-
-
-//_-_-_-_-_-_-_-_-_-_-_-_-_ FUNCTIONS _-_-_-_-_-_-_-_-_-_-_-_-_\\\ 
-
+//  ======================================== FUNCTIONS ========================================
 
 // Function used to set servo to a int value from 0-180
 void setServoAngle(int deg) {
@@ -136,7 +53,7 @@ void gpio_init() {
 }
 
 
-/////_-_-_-_-_-_-_-_-_-_-_-_-_ SETUP _-_-_-_-_-_-_-_-_-_-_-_-_\\\\\ 
+//  ======================================== SETUP ========================================
 void setup() {
 //  (  1 ) Divide servo's 180deg range of motion into 180 / SERVO_STEP
 //  (  2 ) Create array or datatype (ie an object class) to store each angle.
@@ -153,7 +70,7 @@ void setup() {
 }
 
 
-////_-_-_-_-_-_-_-_-_-_-_-_-_ MAIN _-_-_-_-_-_-_-_-_-_-_-_-_\\\\ 
+//  ======================================== MAIN ========================================
 void loop() {
 //  (  1 ) Check if IS_RUNNING = true.
 //  (  2 ) For each step in servo's range of motion, measure distance
@@ -167,9 +84,4 @@ void loop() {
 //  ( 10 ) 
 //  ( 11 ) 
 
-  // put your main code here, to run repeatedly:
-
-
-//      ( 1 ) Divide servo's 180deg range of motion into 180 / SERVO_STEP
-//      ( 2 ) 
 }
