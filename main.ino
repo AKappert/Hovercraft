7eb013b59e1dd167b967951dc5a30ced32959c29
AKappert <andrewkappert@outlook.com>
2022-11-11 00:16:27 -0500

    Initial comit to setup github link

--- main.ino
+++ main.ino
@@ -1,12 +1,55 @@
-/********************************/
-/***********MAIN FILE************/
-/********************************/
+//_-_-_-_-_-_-_-_-_-_-_-_-_ INCLUDES _-_-_-_-_-_-_-_-_-_-_-_-_\\
 
-void setup()
-{
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include <avr/io.h>
+
+
+
+///_-_-_-_-_-_-_-_-_-_-_-_-_ CLASSES _-_-_-_-_-_-_-_-_-_-_-_-_\\\
+
+class cLED {
+  #define LED_PIN PB3
+  #define NANO_LED_PIN PB5
+
+  private:
+     //true = on.
+    bool IS_ON = false;
+  public:
+  
+    //cLED default constructor
+    cLED() {
+
+    }
+    //cLED concstructor with initializer
+    cLED(bool o) {
+      IS_ON = o;
+    }
+};
+
+
+///_-_-_-_-_-_-_-_-_-_-_-_-_ DEFINES _-_-_-_-_-_-_-_-_-_-_-_-_\\\
+
+
+#DEFINE SERVO_STEP 1
+
+
+
+//_-_-_-_-_-_-_-_-_-_-_-_-_ VARIABLES _-_-_-_-_-_-_-_-_-_-_-_-_\\
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
 }
 
-void loop()
-{
-    
+void loop() {
+  // put your main code here, to run repeatedly:
+
+
+//      ( 1 ) Divide servo's 180deg range of motion into 180 / SERVO_STEP
+//      ( 2 ) 
+
+
 }
